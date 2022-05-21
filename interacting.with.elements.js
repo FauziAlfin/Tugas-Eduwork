49cfead1c0696bedbf22966878759662d94476d3
Alfin Fauzi Rizqi Tsani <alfin.fauzi95@gmail.com>
2022-05-21 21:42:01 +0700

    Tugas Section 5 - Interacting With Elements

---
+++ interacting.with.elements.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Working with inputs', () => {
+    it('visit the website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url().should('include', 'login.html')
+    })
+    it("should fill username", () => {
+        cy.get('#user_login').clear()
+        cy.get('#user_login').type('username')
+    });
+    it("should fill password", () => {
+        cy.get('input[name="user_password"]').clear()
+        cy.get('input[name="user_password"]').type(password)
+    });
+
+    it("should check the checkbox", () => {
+        cy.get('checkbox').clear()
+        cy.get('checkbox').check()
+    });
+})
